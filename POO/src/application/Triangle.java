3ffc5409453a7b630eaca9075214754107f1d217
FelipeC-05 <barbosacarvalhodoamaralfelipe@gmail.com>
2024-09-10 23:54:44 -0300

    Introdução a POO

---
+++ POO/src/application/Triangle.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.form_triangle;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		/*
+		 * Fazer um programa para ler as medidas dos lados de dois triângulos X e Y
+		 * (suponha medidas válidas). Em seguida, mostrar o valor das áreas dos dois
+		 * triângulos e dizer qual dos dois triângulos possui a maior área.
+		 */
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		form_triangle x, y;
+		
+		x = new form_triangle();
+		y = new form_triangle();
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println("Enter the measures of triangle Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");
+		}
+		sc.close();
+
+	}
+
+}
