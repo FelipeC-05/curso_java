22a228aa28383edd81172293478e28d70f27902d
FelipeC-05 <barbosacarvalhodoamaralfelipe@gmail.com>
2024-10-03 23:49:03 -0300

    Iniciando enumeração 2

---
+++ enumeracao,composicao 2/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		/*
+		 * Ler os dados de um pedido com N itens (N fornecido pelo usuário). Depois, mostrar um
+		 * sumário do pedido
+		 */
+		Locale.setDefault(Locale.US);	
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.next();
+	}
+
+}
