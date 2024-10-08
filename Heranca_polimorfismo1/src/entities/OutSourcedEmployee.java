5ad7a606e3e9a1885c4a7645d7357e74c9104289
FelipeC-05 <barbosacarvalhodoamaralfelipe@gmail.com>
2024-10-07 23:53:16 -0300

    Aprendendo herança e polimorfismo

---
+++ Heranca_polimorfismo1/src/entities/OutSourcedEmployee.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class OutSourcedEmployee extends Employee {
+	private Double additionalChange;
+	
+	public OutSourcedEmployee() {
+		super();
+	}
+
+	public OutSourcedEmployee(String name, Integer hours, Double valuePerHours, Double additionalChange) {
+		super(name, hours, valuePerHours);
+		this.additionalChange = additionalChange;
+	}
+
+	public Double getAdditionalChange() {
+		return additionalChange;
+	}
+
+	public void setAdditionalChange(Double additionalChange) {
+		this.additionalChange = additionalChange;
+	}
+	
+	@Override
+	public Double payment() {
+		return super.payment() + additionalChange * 1.1;
+	}
+	
+	
+}
